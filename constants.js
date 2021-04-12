7986809e964af68aed51e8d775a50e8995ec1d8c
Vendicated <vendicated@riseup.net>
2021-04-12 03:38:29 +0200

    Add License headers

--- constants.js
+++ constants.js
@@ -1,3 +1,20 @@
+/* WordNotifications, a Powercord plugin that notifies you when someone says specific words
+ * Copyright (C) 2021 Vendicated
+ *
+ * WordNotifications is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WordNotifications is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WordNotifications.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 module.exports = {
   TOAST_TIMEOUT: 5,
   HEADER_FORMAT: "{USER_TAG} mentioned {TRIGGER_COUNT} triggers in #{CHANNEL_NAME}",
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/* WordNotifications, a Powercord plugin that notifies you when someone says specific words
+ * Copyright (C) 2021 Vendicated
+ *
+ * WordNotifications is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WordNotifications is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WordNotifications.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const { Plugin } = require("powercord/entities");
 const { inject, uninject } = require("powercord/injector");
 const { FluxDispatcher } = require("powercord/webpack");
